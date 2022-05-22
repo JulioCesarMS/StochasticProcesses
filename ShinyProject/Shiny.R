89b6743b6e40056fe6a99392bf94cea3e3f815c8
Julio César Martínez Sánchez <53662692+JulioCesarMS@users.noreply.github.com>
2022-05-22 10:52:40 -0500

    Add files via upload

---
+++ ShinyProject/Shiny.R
@@ -0,0 +1,155 @@
+
+#########################   Installing and Reading packages   #################################
+check.packages <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+packages<-c("ggplot2", "dplyr", "tidyverse", "plotly", "ggthemes", "tidyr","shiny","shinydashboard")
+check.packages(packages)
+
+################################################################################
+##############################  Dashboard   ####################################
+################################################################################
+# Create an empty header
+header <- dashboardHeader(title = "Stochastic Processes")
+
+# Create an empty sidebar
+sidebar <- dashboardSidebar(
+  
+  sidebarMenu(
+    menuItem(text = 'Dashboard',
+             tabName = 'dashboard'
+    )
+  ),
+  
+  sliderInput(
+    inputId = "nsteps",
+    label = "n-steps",
+    min = 1,
+    max = 10000,
+    value = 100,
+    animate=animationOptions(10)),
+  
+  sliderInput(
+    inputId = "nrep",
+    label = "n-rep",
+    min = 2,
+    max = 1000,
+    value = 100),
+  
+  sliderInput(
+    inputId = "a",
+    label = "Probability (a)",
+    min = 0,
+    max = 1,
+    value = 0.5),
+  
+  # action buttom
+  actionButton("click", "Update")
+)
+
+# Create an empty body
+body <- dashboardBody(
+  fluidRow(
+   
+    #column 3
+    valueBox(
+      width = 12,
+      value = "Simple Random Walk",
+      subtitle = "UANL", 
+      icon = icon("fire")
+    ), 
+  
+  ),
+  
+  fluidRow(
+    # Row 2
+    box(title = "Simple Random Walk",status = "primary", solidHeader = TRUE,
+        plotOutput('RandomWalk_trayectories',height = 350),width=7,height = "10%"),
+    
+    box(title = "Distribution at time t",status = "primary", solidHeader = TRUE,
+        plotOutput('RandomWalk_distribution',height = 350),width=5,height = "10%"),
+    
+  ),
+  
+  fluidRow(
+    box(title = "2-Dimensional Random Walk",status = "primary",solidHeader= TRUE,
+        plotOutput("RandomWalk_2d",height = 350), width=7),
+    box(title = "3-Dimensional Random Walk",status = "primary",solidHeader= TRUE,
+        plotlyOutput("RandomWalk_3d",height = 350), width=5),
+  ),
+  
+)
+
+
+ui <- dashboardPage(skin = "blue",header,sidebar,body)
+
+server <- function(input,output){
+  # Dataset
+  df1 <- reactive({
+    df <- simple.random.walk(input$nsteps,input$nrep,prob.r=input$a)
+    df
+  }) 
+  
+  output$RandomWalk_trayectories <- renderPlot({
+    df_rw <- df1()  %>%
+      gather(key='t',value='valor',-sim) %>%
+      mutate(t = as.numeric(substring(t,4,10))) %>%
+      arrange(sim)
+    moments_rw <- data.frame('t'=c(1:input$nsteps),'a'=input$a) %>%
+      mutate('mean'=t*(a-(1-a)),
+             'sd_sup'=mean + 2*sqrt(4*t*a*(1-a)),
+             'sd_inf'=mean - 2*sqrt(4*t*a*(1-a))) %>%
+      filter(t <= input$nsteps)
+ 
+    p1 <- ggplot(df_rw,aes(x=t,y=valor,color=sim)) +
+      geom_line() +
+      geom_line(moments_rw, mapping=aes(x=t,y=mean),col='red',size=0.7) +
+      geom_line(moments_rw, mapping=aes(x=t,y=sd_sup),col='blue',size=0.7,linetype = "dashed") +
+      geom_line(moments_rw, mapping=aes(x=t,y=sd_inf),col='blue',size=0.7,linetype = "dashed") +
+      scale_colour_grey(start = 0.2,end = 0.6) +
+      theme(legend.position="none") +
+      xlab("") +
+      ylab("")+
+      ggtitle(paste(input$nrep," Simple Random Walk trajectories",sep=''))
+    p1
+  })
+  
+  output$RandomWalk_distribution <- renderPlot({
+    df_rw <- df1() %>%
+      gather(key='t',value='valor',-sim) %>%
+      mutate(t = as.numeric(substring(t,4,10))) %>%
+      arrange(sim) %>%
+      filter(t <= input$nsteps)
+    t.selected <- input$nsteps
+    df_dist <- df_rw %>% filter(t==t.selected)
+    
+    p2 <- ggplot(df_dist,aes(valor)) +
+      geom_histogram(bins=15,fill='red',col="white") +
+      #coord_flip() +
+      theme(axis.title.x = element_blank(),
+            axis.title.y = element_blank(),
+            axis.text.x=element_blank(),
+            axis.text.y=element_blank(),
+            axis.ticks.x=element_blank(),
+            axis.ticks.y=element_blank())
+    p2  
+  })
+  
+  output$RandomWalk_2d <- renderPlot({
+    
+    p3 <- randomWalk2d_plot(base = df1() ,n.steps = input$nsteps)
+    p3
+  } )
+  
+  output$RandomWalk_3d <- renderPlotly({
+    
+    p4 <- randomWalk3d_plot(base = df1() ,n.steps = input$nsteps)
+    p4
+  } )
+  
+}
+# run app
+#shinyApp(ui, server)
---
+++ ShinyProject/SimpleRandomWalk.R
@@ -0,0 +1,129 @@
+
+################################################################################
+#######################    Simple Random walk               ####################
+################################################################################
+
+##################   Installing and Reading packages   #########################
+check.packages <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+packages<-c("ggplot2", "dplyr", "tidyverse", "plotly", "ggthemes", "tidyr","stringr")
+check.packages(packages)
+
+
+################################################################################
+#########################   Random walk function  ##############################
+################################################################################
+simple.random.walk <- function(n.steps,n.sim,prob.r=0.5){
+  n <- n.steps
+  a <- prob.r
+  
+  x.left = -1
+  x.right = 1
+  
+  Sn_mat <- matrix(0,ncol=n+1,nrow=n.sim)
+  for(i in 1:n.sim){
+    for(j in 2:(n+1)){
+      step <- sample(c(x.left,x.right),1,prob=c(1-a,a),replace=F)
+      Sn_mat[i,j] <- Sn_mat[i,j-1] + step  
+    }
+  }
+  # nombre de columnas
+  names <- sapply(1:(n+1),function(i) paste('step',i,sep=''))
+  # data frame
+  result_df <- data.frame('sim'=sapply(1:n.sim, function(i) paste('sim',i,sep='')),
+                   'Sn'=Sn_mat)
+  return(result_df)
+}
+################################################################################
+#######  Ejemplo (app):
+n.steps <- 10000   # número de pasos
+n.sim <- 1000     # número de trajectorias
+a <- 0.5          # probabilidad a la derecha
+
+# simulación
+df <- simple.random.walk(n.steps,n.sim,prob.r=a)
+
+##  Base para gráfico
+df_rw <- df  %>%
+  gather(key='t',value='valor',-sim) %>%
+  mutate(t = as.numeric(substring(t,4,10))) %>%
+  arrange(sim)
+
+moments_rw <- data.frame('t'=c(1:n.steps),'a'=a) %>%
+  mutate('mean'=t*(a-(1-a)),
+         'sd_sup'=mean + 2*sqrt(4*t*a*(1-a)),
+         'sd_inf'=mean - 2*sqrt(4*t*a*(1-a)))
+
+# Gráfico de trajectorias  
+p1 <- ggplot(df_rw,aes(x=t,y=valor,color=sim)) +
+  geom_line() +
+  geom_line(moments_rw, mapping=aes(x=t,y=mean),col='red',size=0.7) +
+  geom_line(moments_rw, mapping=aes(x=t,y=sd_sup),col='blue',size=0.7,linetype = "dashed") +
+  geom_line(moments_rw, mapping=aes(x=t,y=sd_inf),col='blue',size=0.7,linetype = "dashed") +
+  scale_colour_grey(start = 0.2,end = 0.6) +
+  theme(legend.position="none") +
+  ggtitle(paste(n.sim," trayectorias del camino aleatorio simple.",sep=''))
+
+
+## Distribución al tiempo t
+t.selected <- 1000
+
+df_dist <- df_rw %>% filter(t==t.selected)
+
+p2 <- ggplot(df_dist,aes(valor)) +
+  geom_histogram(bins=20,fill='red',col="white") +
+  ggtitle(paste("Distribución al tiempo t = ",t.selected,sep="")) +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.ticks.y=element_blank())
+
+################################################################################
+##################### Caminata simple en dos dimensiones  ######################
+################################################################################
+randomWalk2d_plot <- function(base, n.steps){
+  
+  df <- base
+  df_2d <- df  %>%
+    gather(key='t',value='valor',-sim) %>%
+    filter(sim == 'sim1' | sim=='sim2') %>%
+    spread(sim,valor)  %>%
+    mutate(t = as.numeric(substring(t,4,10))) %>%
+    arrange(t) %>%
+    filter(t <= n.steps)
+  b2 <- ggplot(df_2d,aes(x=sim1,y=sim2))+
+    geom_point(color="blue") +
+    geom_point(df_2d%>%filter(t == 1),mapping=aes(x=sim1,y=sim2),color="green") +
+    geom_point(df_2d%>%filter(t == max(t)),mapping=aes(x=sim1,y=sim2),color="red") +
+    geom_path() +
+    theme(axis.title.x = element_blank(),
+          axis.title.y = element_blank(),
+          axis.text.x=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks.x=element_blank(),
+          axis.ticks.y=element_blank())
+  return(b2)
+}
+
+################################################################################
+################## Caminata simple en tres dimensiones  ########################
+################################################################################
+randomWalk3d_plot <- function(base, n.steps){
+  
+  df <- base
+  df_2d <- df  %>%
+    gather(key='t',value='valor',-sim) %>%
+    filter(sim == 'sim1' | sim=='sim2' | sim == 'sim3') %>%
+    spread(sim,valor)  %>%
+    mutate(t = as.numeric(substring(t,4,10))) %>%
+    arrange(t) %>%
+    filter(t <= n.steps)
+  b3 <- plot_ly(x=df_2d$sim1, y=df_2d$sim2, z=df_2d$sim3, type="scatter3d", mode="lines")
+  
+  return(b3)
+}
+
